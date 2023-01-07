dc325a26b95a336b19b49b6356d77ce6a0bb98aa
Hatrison <vadimulanovskiy@gmail.com>
2023-01-07 18:36:30 +0200

    Added enemies moving

--- src/js/Enemy.js
+++ src/js/Enemy.js
@@ -5,8 +5,12 @@ export default class Enemy {
     this.x = x;
     this.y = y;
     this.size = size;
-    this.spped = speed;
+    this.speed = speed;
     this.map = map;
+
+    this.direction = this.#newDirection();
+    this.directionTimerDefault = this.#random(10, 30);
+    this.directionTimer = this.directionTimerDefault;
 
     this.normalGhost = new Image();
     this.normalGhost.src = "../../images/ghost.png";
@@ -21,6 +25,55 @@ export default class Enemy {
   }
 
   draw(ctx) {
+    this.#move();
+    this.#changeDirection();
     ctx.drawImage(this.ghostImage, this.x, this.y, this.size, this.size);
+  }
+
+  #changeDirection() {
+    this.directionTimer--;
+    let newDirection = null;
+    if (this.directionTimer === 0) {
+      this.directionTimer = this.directionTimerDefault;
+      newDirection = this.#newDirection();
+    }
+
+    if (newDirection !== null && this.direction !== newDirection) {
+      if (
+        Number.isInteger(this.x / this.size) &&
+        Number.isInteger(this.y / this.size)
+      ) {
+        if (!this.map.isCollision(this.x, this.y, newDirection)) {
+          this.direction = newDirection;
+        }
+      }
+    }
+  }
+
+  #move() {
+    if (!this.map.isCollision(this.x, this.y, this.direction)) {
+      switch (this.direction) {
+        case directions.up:
+          this.y -= this.speed;
+          break;
+        case directions.right:
+          this.x += this.speed;
+          break;
+        case directions.down:
+          this.y += this.speed;
+          break;
+        case directions.left:
+          this.x -= this.speed;
+          break;
+      }
+    }
+  }
+
+  #newDirection() {
+    return Math.floor(Math.random() * Object.keys(directions).length);
+  }
+
+  #random(min, max) {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 }
